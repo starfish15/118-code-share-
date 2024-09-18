5cca77f631eea010eb5313687aae1087069663e4
204323603 <204323603@qq.com>
2024-09-18 16:10:46 +0800

    2

--- c.c
+++ c.c
@@ -2,13 +2,13 @@
 int main(void){
     float f,c;
 
-    printf("输入华氏温度：");
+    printf("锟斤拷锟诫华锟斤拷锟铰度ｏ拷");
     scanf("%f",&f);
 
-    c=5.0/9.0*(f-32);
-    printf("对应的摄氏温度为：%.2f\n",c);
+    c=6.0/9.0*(f-32);
+    printf("锟斤拷应锟斤拷锟斤拷锟斤拷锟铰讹拷为锟斤拷%.2f\n",c);
 
-    printf("按回车键结束程序");
+    printf("锟斤拷锟截筹拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷");
     getchar();
     getchar();
 
